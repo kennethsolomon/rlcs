1b4f1bfb3ce9c9c8ddffe6888f29d72bb1f91736
Kenneth Solomon <synecders@gmail.com>
2020-01-31 10:45:10 +0800

    fix project_receivable error

--- pages/cashier/savesales.php
+++ pages/cashier/savesales.php
@@ -16,9 +16,10 @@ $date = date('m-d-Y');
 $dmonth = date('F');
 $dyear = date('Y');
 
-$isChecked = $_POST['isChecked'];
+
 
 if($d=='credit') {
+	$isChecked = $_POST['isChecked'];
 	$sql = "UPDATE sales_order 
     SET status=''
     WHERE invoice=?"; 
@@ -32,7 +33,21 @@ if($d=='credit') {
 	header("location: preview.php?invoice=$a&isChecked=$isChecked");
 	exit();
 }
+if($d=='project_receivable') {
+	$sql = "UPDATE sales_order 
+    SET status=''
+    WHERE invoice=?"; 
+    $q = $db->prepare($sql);
+	$q->execute(array($a));
+	
+	$sql = "INSERT INTO sales (invoice_number,cashier,date,type,total_amount,name,month,year,balance,p_amount,vat,address, contact_number, profit) VALUES (:a,:b,:c,:d,:e,:g,:h,:i,:k,:j,:l, :m, :z, :profit)";
+	$q = $db->prepare($sql);
+	$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':g'=>$cname,':h'=>$dmonth,':i'=>$dyear,':k'=>$e,':j'=>$pamount,':l'=>$vat,':m'=>$caddress, ':z'=>$ccontact, ':profit'=>$profit));
+	header("location: preview.php?invoice=$a");
+	exit();
+}
 if($d=='cash') {
+	$isChecked = $_POST['isChecked'];
 	$sql = "UPDATE sales_order 
     SET status=''
     WHERE invoice=?"; 
