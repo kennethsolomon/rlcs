0f5d702630ec35279d7c6b581a730b1145a98ae0
Kenneth Lim Solomon <synecders@gmail.com>
2020-02-06 16:18:24 +0800

    fix error on customer.php when adding new customer

--- pages/savecustomer.php
+++ pages/savecustomer.php
@@ -6,7 +6,7 @@ $b = $_POST['address'];
 $c = $_POST['contact'];
 $d = $_POST['memno'];
 // query
-$sql = "INSERT INTO customer (first_name,address,contact,membership_number,last_name,middle_name,customer_name) VALUES (:a,:b,:c,:d,:h)";
+$sql = "INSERT INTO customer (first_name,address,contact,membership_number,customer_name) VALUES (:a,:b,:c,:d,:e)";
 $q = $db->prepare($sql);
-$q->execute(array(':a' => $a, ':b' => $b, ':c' => $c, ':d' => $d, ':h' => $a));
+$q->execute(array(':a' => $a, ':b' => $b, ':c' => $c, ':d' => $d, ':e' => $a));
 header("location: customer.php");
